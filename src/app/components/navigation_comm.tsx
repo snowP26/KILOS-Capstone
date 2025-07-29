d2d2e024a520fd16f4cddd94300643bd1772a3c8
snowP_ <jverceluz@gbox.adnu.edu.ph>
2025-07-29 20:13:26 +0800

    chore: community navbar v1.0

--- src/app/components/navigation_comm.tsx
+++ src/app/components/navigation_comm.tsx
@@ -1,21 +1,26 @@
 "use client";
 
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+// Created by James, this is the navigation bar component for the community side view. 
 
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 
 export const ComNav = () => {
-    const pathname = usePathname();
+    const pathname = usePathname(); // use this to check the current tab the user is on.
+    const router = useRouter(); // use this for /app navigation
 
     return(
-        <nav>
-            <Link href='/' className={pathname === "/" ? "mr-4 text-white font-bold" : "mr-4 text-gray-500"}>
-                Home
-            </Link>
-            <Link href='/feedback' className={pathname === "/feedback" ? "mr-4 text-white font-bold" : "mr-4 text-gray-500"}>
-                Share your Insights
-            </Link>
-            <Link href='/login' className={pathname === "/login" ? "mr-4 text-white font-bold" : "mr-4 text-gray-500"}>
+        <nav className="flex items-center justify-between py-5 px-20 border-b-2 sticky top-0 bg-white z-50">
+            <h1 className="inline-block text-[#0073FF] font-bold text-3xl cursor-pointer" onClick={()=> {router.push("/")}}>KILOS</h1>
+            <div className="flex space-x-6">
+                <Link href='/' className={pathname === "/" ? "mr-4 text-black font-bold text-lg" : "mr-4 text-gray-500 text-lg"}>
+                    Home
+                </Link>
+                <Link href='/feedback' className={pathname === "/feedback" ? "mr-4 text-black font-bold text-lg" : "mr-4 text-gray-500 text-lg"}>
+                    Share your Insights
+                </Link>
+            </div>
+            <Link href='/login' className={pathname === "/login" ? "mr-4 text-black font-bold text-md" : "mr-4 text-gray-500 text-md"}>
                 Login as Official
             </Link>
         </nav>
