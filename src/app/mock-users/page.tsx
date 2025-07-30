d0cb820ee48a2ecf506be1d2bdf9b58eec5a4ba3
snowP_ <jverceluz@gbox.adnu.edu.ph>
2025-07-30 22:53:03 +0800

    chore: supabase CRD functions

--- src/app/mock-users/page.tsx
+++ src/app/mock-users/page.tsx
@@ -1,59 +1,90 @@
-import { revalidatePath } from "next/cache";
+'use client';
 
+import client from "@/src/api/client";
+import { useEffect, useState } from "react";
 
-
-type MockUsers = {
+type u = {
     id: number;
-    name: string
+    name: string;
 }
 
-export default async function MockUsers() {
-    const res = await fetch("https://688982db4c55d5c739528c61.mockapi.io/users")
-    const users = await res.json();
 
-    async function addUser(formData: FormData) {
-        "use server";
-        const name = formData.get("name");
-        const res = await fetch("https://688982db4c55d5c739528c61.mockapi.io/users", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ name }),
-        });
-        const newUser = await res.json();
-        revalidatePath("/mock-users")
-        console.log(newUser)
+
+export default function MockUsers() {
+    
+    const fetchUsers = async() =>{
+        const data = await getUsers();
+        if (data) setUsers(data);
+    }
+    const [newUser, setNewUser] = useState({ name: "" });
+    const [users, setUsers] = useState<u[]>([]);
+
+    const getUsers = async() => {
+        const { data, error } = await client.from("user-test").select("*");
+
+        if(error){
+            return console.error("Error in retrieving the user: ", error.message);
+        }
+
+        return data;
     }
 
-    async function deleteUser(formData: FormData){
-        "use server";
-        const id = formData.get("id");
+    const deleteUsers = async(id: number) => {
         
+        const { error } = await client.from("user-test").delete().eq("id", id);
+
+        if(error){
+            return console.error(error);
+        }
+
+        console.log("Delete user successful");
+        fetchUsers();
     }
+    
+    const handleSubmit = async(e: any) => {
+        e.preventDefault();
 
+        if(newUser.name === ""){
+            return console.error("Name should not be empty!")
+        }
 
+        const { error } = await client.from("user-test").insert(newUser);
+
+        if(error){
+            return console.error("Error in creating the user: ", error.message);
+        }
+
+        console.info("Successfully added the user!")
+        setNewUser({ name: "" });
+        const updatedUsers = await getUsers();
+        if (updatedUsers) setUsers(updatedUsers);
+    
+    }
+    useEffect(() => {
+        fetchUsers();
+    }, []); 
 
     return (
         <div className="py-10 px-4 max-w-6xl mx-auto">
-            <form action={addUser} className="mb-8 flex flex-col sm:flex-row items-start sm:items-center gap-4">
+            <form className="mb-8 flex flex-col sm:flex-row items-start sm:items-center gap-4">
                 <input
                     type="text"
                     name="name"
                     required
+                    onChange={(e) => setNewUser({name: e.target.value})}
                     placeholder="Enter user name"
                     className="border border-gray-300 rounded-lg px-4 py-2 w-full sm:w-auto"
                 />
                 <button
                     type="submit"
-                    className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition-colors duration-200"
+                    onClick={handleSubmit}
+                    className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition-colors duration-200 cursor-pointer"
                 >
                     Add User
                 </button>
-            </form>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                {users.map((user: MockUsers) => (
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                {users.map((user: u) => (
                     <div key={user.id} className="p-4 bg-gray-900 shadow-md rounded-xl text-white flex flex-col justify-between h-full">
                         <span className="text-lg font-semibold mb-4 break-words">{user.name}</span>
                         <div className="flex justify-end gap-2 mt-auto">
@@ -66,6 +97,7 @@ export default async function MockUsers() {
                             <button
                                 type="button"
                                 className="bg-red-500 text-white rounded-lg px-3 py-1 text-sm hover:bg-red-600 transition duration-200"
+                                onClick={() => deleteUsers(user.id)}
                             >
                                 Delete
                             </button>
@@ -73,6 +105,7 @@ export default async function MockUsers() {
                     </div>
                 ))}
             </div>
+            </form>
         </div>
     );
 }
