d59bc797b8488cb32ae501565c9893308306c0ca
snowP_ <jverceluz@gbox.adnu.edu.ph>
2025-08-01 17:20:04 +0800

    chore: update landing page ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 import { ComNav } from "./components/community/nav";
 import { Title } from "./components/community/title";
+import { Events } from "./components/community/upcoming-events";
 
 export default function Home() {
 
   return (
-    <div>
+    <div className="pb-16">
       <ComNav />
       <div className="min-h-screen overflow-y-auto">
         <Title />
-        <h1>UPCOMING EVENTS</h1>
+        <Events />
       </div>
     </div>
   );
