a350d04fec0d1aceacd69c2e91d571c2df1dd021
snowP26 <jverceluz@gbox.adnu.edu.ph>
2025-08-04 14:55:23 +0800

    chore: move to pc

--- src/app/(screens)/feedback/[id]/page.tsx
+++ src/app/(screens)/feedback/[id]/page.tsx
@@ -3,12 +3,14 @@ import { CommunityBanner } from "@/src/app/components/community/community-banner
 import { FeedbackCard } from "@/src/app/components/community/feedbackCard"
 import { Button } from "@/components/ui/button"
 
-const page = () => {
+const page = ({ params } : { params: { id: string }}) => {
+
+
   return (
     <div>
         <ComNav />
         <div className="mt-10">
-            <CommunityBanner />
+            <CommunityBanner params={params}/>
         </div>
 
         <p className="text-4xl font-bold text-center">Community Feedback</p>
