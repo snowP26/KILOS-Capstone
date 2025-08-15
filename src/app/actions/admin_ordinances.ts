d76f123a2057688e24872acb52a365401cae8a36
snowP26 <jverceluz@gbox.adnu.edu.ph>
2025-08-15 23:15:44 +0800

    feat: add other ordinance functions on the admin side

--- src/app/actions/admin_ordinances.ts
+++ src/app/actions/admin_ordinances.ts
@@ -1,4 +1,5 @@
 import client from "@/src/api/client"
+import { ordinance } from "../lib/definitions";
 
 const emailToUserID = async (email: string) => {
     const id = await client.from("youth_official").select("id").eq("email", email);
@@ -26,5 +27,28 @@ export const viewOwnOrdinance = async () => {
 
     if(error) return console.log("Error matching UserID to an ordinance: ", error);
 
+    return data;
+}
+
+export const getOrdinanceByName = async (title: string) => {
+    const { data, error } = await client.from("ordinances").select("*").eq("title", title).limit(1).single();
+
+    if(error) {
+        console.log("Error fetching the data: ", error)
+        return null
+    }
+
+    console.log("Ordinance found: ", data);
+    return data as ordinance;
+}
+
+export const getFilesPerOrdinance = async (id: number) => {
+    const { data, error } = await client.from("ordinance_files").select("*").eq("ordinance_id", id);
+
+    if(error){
+        return console.log("Error retrieving files: ", error)
+    }
+
+    return console.log("files found: ", data)
     return data;
 }
