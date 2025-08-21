0a14dfad7cae0f4219e05953d2ad8298cd433338
snowP26 <jverceluz@gbox.adnu.edu.ph>
2025-08-21 17:17:45 +0800

    chore: fix ui elements issues

--- src/app/components/community/feedbackCard.tsx
+++ src/app/components/community/feedbackCard.tsx
@@ -18,27 +18,32 @@ type feedbackCardProps = {
   header: string;
   body: string;
   date: string;
+  isWhite: boolean;
 };
 
-export const FeedbackCard = ({ header, body, date }: feedbackCardProps) => {
+export const FeedbackCard = ({ header, body, date, isWhite }: feedbackCardProps) => {
+  const bgColor = isWhite ? "bg-white" : "bg-[#E6F1FF]";
+
   return (
     <Dialog>
       <DialogTrigger className="m-10">
-        <div className="cursor-pointer p-5 h-100 w-100 bg-[#E6F1FF] rounded-md shadow-lg transform transition-all duration-300 hover:-translate-y-2 hover:shadow-2xl">
-          <Quote fill='black' />
+        <div
+          className={`cursor-pointer p-5 h-100 w-100 ${bgColor} rounded-md shadow-lg transform transition-all duration-300 hover:-translate-y-2 hover:shadow-2xl`}
+        >
+          <Quote fill="black" />
           <p className="m-5 font-semibold text-4xl">{header}</p>
 
           <p className="m-5 text-gray-500 text-sm">{body}</p>
 
           <div className="flex flex-row justify-end">
             <p className="font-thin text-xs mr-5 content-center">{date}</p>
-            <Quote fill='black' />
+            <Quote fill="black" />
           </div>
         </div>
       </DialogTrigger>
-      <DialogContent className="bg-[#E6F1FF]">
+      <DialogContent className={bgColor}>
         <DialogHeader>
-          <Quote fill='black' size="40px"/>
+          <Quote fill="black" size="40px" />
           <DialogTitle className="text-3xl text-center">{header}</DialogTitle>
           <p className="text-justify mt-5">{body}</p>
           <div className="flex flex-row text-xs gap-2 mt-5 mb-5 justify-end">
@@ -62,17 +67,19 @@ export const FeedbackCard = ({ header, body, date }: feedbackCardProps) => {
               </div>
             </div>
             <div className="w-[80%] bg-white rounded-xl p-3 text-black text-justify">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
                 nisi ut aliquip ex ea commodo consequat.
               </p>
             </div>
           </div>
           {/* end of Comments */}
-          <Quote fill='black' size="40px" className="self-end mt-5" />
+          <Quote fill="black" size="40px" className="self-end mt-5" />
         </DialogHeader>
       </DialogContent>
     </Dialog>
-  )
-}
+  );
+};
+
