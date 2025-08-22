d5db6d82020552fa1efdf2fe577d9edfda623c1f
snowP_ <jverceluz@gbox.adnu.edu.ph>
2025-08-23 01:55:46 +0800

    chore: add author id to author name conversion

--- src/app/actions/convert.ts
+++ src/app/actions/convert.ts
@@ -6,9 +6,9 @@ export const locNameToID = async (loc: string) => {
     console.log(`Loc: ${loc}`)
     console.log(`Data: ${data}`)
 
-    if(error || data==null) {
+    if (error || data == null) {
         return console.log("Data not found: ", error)
-    } 
+    }
 
 
     return data[0].id as number
@@ -17,20 +17,35 @@ export const locNameToID = async (loc: string) => {
 export const getLocFromAuth = async () => {
     const { data: userData, error: userError } = await client.auth.getUser();
 
-    if(!userData){
+    if (!userData) {
         return console.log("No authenticated user is found.")
-    } 
+    }
 
-    if(userError){
+    if (userError) {
         return console.log("User error: ", userError)
     }
 
     const authEmail = userData.user.email;
     const { data, error } = await client.from("youth_official").select("location").eq("email", authEmail).single();
-    
-    if(error){
+
+    if (error) {
         return console.log("There seems to be an error with the youth official", error)
     }
 
     return data.location as number;
 }
+
+export const authorIDtoName = async (id: number) => {
+    const { data, error } = await client.from("youth_official").select("firstname, lastname").eq("id", id).single()
+
+    if (error) {
+        console.error("Error fetching author name:", error);
+        return null; // or throw error if you want stricter handling
+    }
+
+    if (data) {
+        return `${data.firstname} ${data.lastname}`;
+    }
+
+    return null;
+};
