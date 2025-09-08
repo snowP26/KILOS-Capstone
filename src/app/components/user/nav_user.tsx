616195353c13c151a38c6298c86faa2542886be4
snowP_ <jverceluz@gbox.adnu.edu.ph>
2025-09-09 00:26:52 +0800

    feat: update Title component to accept onLocationChange prop and modify LocationSelect usage fix: change Link components to anchor tags in UserNav for navigation

--- src/app/components/user/nav_user.tsx
+++ src/app/components/user/nav_user.tsx
@@ -39,24 +39,24 @@ export const UserNav = () => {
             <div className="container w-full flex mx-5 justify-between sm:items-center md:justify-between lg:justify-around xl:w-[90%] gap-30">
                 <TextIcon />
                 <div className={getMenuClassname()}>
-                    <Link href='/users/home' className={pathname.startsWith('/users/home') ? 'p-4 bg-white text-[#052659] rounded-md font-bold' : "text-white"}>
+                    <a href='/users/home' className={pathname.startsWith('/users/home') ? 'p-4 bg-white text-[#052659] rounded-md font-bold' : "text-white"}>
                         Home
-                    </Link>
-                    <Link href='/users/projects' className={pathname.startsWith('/users/projects') ? 'p-4 bg-white text-[#052659] rounded-md font-bold' : "text-white"}>
+                    </a>
+                    <a href='/users/projects' className={pathname.startsWith('/users/projects') ? 'p-4 bg-white text-[#052659] rounded-md font-bold' : "text-white"}>
                         Projects
-                    </Link>
-                    <Link href='/users/ordinances' className={pathname.startsWith('/users/ordinances') ? 'p-4 bg-white text-[#052659] rounded-md font-bold' : "text-white"}>
+                    </a>
+                    <a href='/users/ordinances' className={pathname.startsWith('/users/ordinances') ? 'p-4 bg-white text-[#052659] rounded-md font-bold' : "text-white"}>
                         Ordinances
-                    </Link>
-                    <Link href='/users/announcements' className={pathname.startsWith('/users/announcements') ? 'p-4 bg-white text-[#052659] rounded-md font-bold' : "text-white"}>
+                    </a>
+                    <a href='/users/announcements' className={pathname.startsWith('/users/announcements') ? 'p-4 bg-white text-[#052659] rounded-md font-bold' : "text-white"}>
                         Announcement
-                    </Link>
-                    <Link href='/users/facebook-page' className={pathname.startsWith('/users/facebook') ? 'p-4 bg-white text-[#052659] rounded-md font-bold' : "text-white"}>
+                    </a>
+                    <a href='/users/facebook-page' className={pathname.startsWith('/users/facebook') ? 'p-4 bg-white text-[#052659] rounded-md font-bold' : "text-white"}>
                         Facebook Page
-                    </Link>
-                    <Link href='/users/community-feedback' className={pathname.startsWith('/users/community-feedback') ? 'p-4 bg-white text-[#052659] rounded-md font-bold' : "text-white"}>
+                    </a>
+                    <a href='/users/community-feedback' className={pathname.startsWith('/users/community-feedback') ? 'p-4 bg-white text-[#052659] rounded-md font-bold' : "text-white"}>
                         Community Feedback
-                    </Link>
+                    </a>
                     <a className="text-white mt-10 lg:m-0 cursor-pointer" onClick={() => logoutUser(router)}>
                         Log Out
                     </a>
