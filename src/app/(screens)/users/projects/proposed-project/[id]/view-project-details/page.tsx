a17a261d91b9ddb61e3135b373389eef2069a342
snowP_ <jverceluz@gbox.adnu.edu.ph>
2025-09-10 22:56:07 +0800

    fix: remove unused import and correct line break in project details

--- src/app/(screens)/users/projects/proposed-project/[id]/view-project-details/page.tsx
+++ src/app/(screens)/users/projects/proposed-project/[id]/view-project-details/page.tsx
@@ -3,7 +3,7 @@
 import React from 'react'
 import { useRouter } from 'next/navigation';
 import { Button } from '@/components/ui/button'
-import { ArrowLeft, SquarePen, Trash2, MessageSquareWarning } from 'lucide-react';
+import { ArrowLeft, SquarePen, Trash2} from 'lucide-react';
 import { Separator } from "@/components/ui/separator"
 import {
     Breadcrumb,
@@ -70,7 +70,7 @@ export default function ViewProposedProject() {
                                     Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
                                     <br /><br />
                                     Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
-                                    <br /><br />
+                                    <br /   ><br />
                                     Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
                                     Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
                                 </p>
