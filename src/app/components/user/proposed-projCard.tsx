b9d19794b9fa1a538fba5e1e08283230e8c6acd8
snowP_ <jverceluz@gbox.adnu.edu.ph>
2025-09-10 15:42:53 +0800

    feat: add props to ProposedProjCard component for dynamic content rendering

--- src/app/components/user/proposed-projCard.tsx
+++ src/app/components/user/proposed-projCard.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-export const ProposedProjCard = () => {
+type ProposedProps = {
+    Title: string,
+    Description: string,
+    Status: string
+}
+
+export const ProposedProjCard = ({ Title, Description, Status }: ProposedProps) => {
     return (
         <div className="bg-[#E6F1FF] h-[100%] w-full cursor-pointer rounded-2xl px-5 py-3 mb-2 shadow-lg transform transition-all duration-300 hover:-translate-y-2 hover:shadow-2xl">
             <div className="flex flex-row gap-3">
@@ -8,14 +14,9 @@ export const ProposedProjCard = () => {
                     {/* image placeholder */}
                 </div>
                 <div className="flex flex-col max-w-[70%] h-[100%] gap-1">
-                    <p className="font-semibold truncate">Title of Proposed Project</p>
-                    <p className="text-xs h-13 w-[100%] max-h-15 line-clamp-3">Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
-                    Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
-                    Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
-                    Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
-                    Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
-                    </p>
-                    <p className="text-red-500 text-xs w-fit px-3 bg-white rounded-2xl">For Approval</p>
+                    <p className="font-semibold truncate">{Title}</p>
+                    <p className="text-xs h-13 w-[100%] max-h-15 line-clamp-3">{Description}</p>
+                    <p className="text-red-500 text-xs w-fit px-3 bg-white rounded-2xl">{Status}</p>
                 </div>
             </div>
         </div>
