6b14b7b722a286de25130056805a6a5a97444b7d
snowP_ <jverceluz@gbox.adnu.edu.ph>
2025-09-19 10:11:10 +0800

    fix: routing

--- src/app/(screens)/users/projects/page.tsx
+++ src/app/(screens)/users/projects/page.tsx
@@ -141,7 +141,7 @@ export default function Projects() {
                         <div className="flex flex-wrap justify-center gap-5 lg:gap-3 xl:gap-5">
                             {projects?.map((data) => (
                                 <div
-                                    onClick={() => router.push(`/users/projects/${data.title}-${data.id}`)}
+                                    onClick={() => router.push(`/users/projects/${(data.title).trim()}-${data.id}`)}
                                     key={data.id}
                                 >
                                     <ProjectCard
